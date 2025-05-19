811782e931c8fe55cd43998be70e39ffb3a1ca43
SabirvAzimboy <sobirov.as.azimboy@gmail.com>
2025-05-19 16:35:02 +0300

    17
    17

--- Demirbaslar.API/Program.cs
+++ Demirbaslar.API/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddSwaggerGen();
 
 // Настройка DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Настройка Identity
 builder.Services.AddIdentity<AppUsers, IdentityRole>()
